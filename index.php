4fc926f4d667f48f4400a37cca634bf124f88c62
Phpswitch <71148614+Phpswitch@users.noreply.github.com>
2020-09-11 22:07:46 +0300

    Update index.php
    i make a change to field names

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
         <title>phpswitch</title>
    </head>
    <body>
-       <input type="text" name="fname" >
+       <input type="text" name="fname" ><br>
+        <input type="text" name="lname" ><br>
+        <input type="email" name="mail" ><br>
+        <input type="password" name="pwd" ><br> 
+       <input type="button" name="btn" >
    </body>
 </html>
